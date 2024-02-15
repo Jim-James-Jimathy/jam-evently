89d2d02d705674de8611ba07792579f62e64fc97
James Kirkwood <jameskwebdev@gmail.com>
2024-02-15 16:46:21 -0500

    fix again

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,6 @@ const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "utfs.io",
-      port: "",
-    },
-  ],
 };
 
 export default nextConfig;
